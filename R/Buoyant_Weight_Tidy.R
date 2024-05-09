9d55e47f3ea50b8e050291df68623f7471089328
CallieStephenson <callies@hawaii.edu>
2024-05-09 12:08:02 -1000

    Adding a 'grams per gram' metric per NS rec

--- R/Buoyant_Weight_Tidy.R
+++ R/Buoyant_Weight_Tidy.R
@@ -33,11 +33,13 @@ BwPac <- Buoyant_weight_Pac_raw %>%
   mutate(T0_dry_weight.g = T0_Weight / (1 - (T0_sw_dens/2.93))) %>% # dry weight of object = weight in water / (1 - (Density of water / Density of object)) (Jokiel et al. 1978)
   mutate(T1_dry_weight.g = T1_Weight / (1 - (T1_sw_dens/2.93))) %>% # dry weight of object = weight in water / (1 - (Density of water / Density of object)) (Jokiel et al. 1978)
   mutate(Change = T1_dry_weight.g - T0_dry_weight.g) %>%
-  mutate(Percent_Change = Change / T0_dry_weight.g) %>%
-  mutate(Percent_Change_100 = Percent_Change * 100) %>%
+  mutate(Proportional_Change = Change / T0_dry_weight.g) %>%
+  mutate(Percent_Change = Proportional_Change * 100) %>%
+  mutate(Change_per_day = Percent_Change/61) %>% 
   mutate(Change_Over_Area= Change / Surface.Area) %>%
-  select(Vial_No_Only, Placement_Code, T0_dry_weight.g, T1_dry_weight.g, Change, Change_Over_Area, Percent_Change) %>% 
-  mutate(Species="PAC")
+  mutate(Species="PAC") %>% 
+ dplyr::select(Vial_No_Only, Placement_Code, T0_dry_weight.g, T1_dry_weight.g, Change, Change_Over_Area, Percent_Change, Change_per_day, Species)
+
 
 #write_csv(BwPac, here("output", "Skeletal_Dry_Weight_Calc_Pac.csv"))
 #############################
@@ -53,10 +55,11 @@ BwPru <- Buoyant_weight_Pru_raw %>%
   mutate(T0_dry_weight.g = T0_Weight / (1 - (T0_sw_dens/2.93))) %>% # dry weight of object = weight in water / (1 - (Density of water / Density of object)) (Jokiel et al. 1978)
   mutate(T1_dry_weight.g = T1_Weight / (1 - (T1_sw_dens/2.93))) %>% # dry weight of object = weight in water / (1 - (Density of water / Density of object)) (Jokiel et al. 1978)
   mutate(Change = T1_dry_weight.g - T0_dry_weight.g) %>%
-  mutate(Percent_Change = Change / T0_dry_weight.g) %>%
-  mutate(Percent_Change_100 = Percent_Change * 100) %>%
+  mutate(Proportional_Change = Change / T0_dry_weight.g) %>%
+  mutate(Percent_Change = Proportional_Change * 100) %>%
+  mutate(Change_per_day = Percent_Change/66) %>% 
   mutate(Change_Over_Area= Change / Surface_Area) %>%
-  select(Vial_No_Only, Placement_Code, T0_dry_weight.g, T1_dry_weight.g, Change, Change_Over_Area, Percent_Change) %>% 
+  dplyr::select(Vial_No_Only, Placement_Code, T0_dry_weight.g, T1_dry_weight.g, Change, Change_Over_Area, Percent_Change, Change_per_day) %>% 
   mutate(Species="PRU")
 
 #write_csv(BwPru, here("output", "Skeletal_Dry_Weight_Calc_Pru.csv"))
